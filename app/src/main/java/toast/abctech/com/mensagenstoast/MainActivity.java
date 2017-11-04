a496e1b57213404282db42301bf34ac3f9451019
Reginaldo Barros de Sousa <32406036+revelarock@users.noreply.github.com>
2017-11-04 00:47:26 -0200

    Initial commit

---
+++ app/src/main/java/toast/abctech/com/mensagenstoast/MainActivity.java
@@ -0,0 +1,28 @@
+package toast.abctech.com.mensagenstoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.botaoId);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Opa blz?", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
